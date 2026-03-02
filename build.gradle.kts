f07a2462fb603b7e76a542ba62929e1f78db2547
Daria <86983122+daria-smirnovaa@users.noreply.github.com>
2026-03-02 14:55:15 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ publishing {
             from(components["java"])
             groupId = "com.github.daria-smirnovaa"
             artifactId = "common"
-            version = "v1.0.0"
+            version = "1.0.0"
         }
     }
 }
